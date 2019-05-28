dcd03157151528fbda7bbbda9a071aa58b0340de
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-05-28 16:53:31 +0300

    add script for support lua-format in vim

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # vim-lua-format
+
+Support for vim for [LuaFormatter](https://github.com/Koihik/LuaFormatter).
+
+## Install
+
+* you have to add next lines in your `.vimrc` file:
+
+  `function! LuaFormat()`
+  `  pyf /usr/local/bin/lua-format.py`
+  `endfunction`
+  `autocmd FileType lua nnoremap <buffer> <c-k> :call LuaFormat()<cr>`
+  `autocmd BufWrite *.lua call LuaFormat()`
+
+## Features
+
+Reformats your Lua source code.
+
+## Extension Settings
+
+* `.lua-format`: Specifies the style config file. [Style Options](https://github.com/Koihik/LuaFormatter/wiki/Style-Config)
---
+++ lua-format.py
@@ -0,0 +1,92 @@
+# based on clang-format.py
+
+from __future__ import absolute_import, division, print_function
+
+import difflib
+import platform
+import subprocess
+import sys
+import vim
+import os
+
+def findConfigFile(name, path):
+    for curFile in os.listdir(path):
+        retval = False
+        if name == curFile:
+            retval = os.path.join(path, name)
+            break
+    if retval:
+        return os.path.abspath(retval)
+    elif os.path.realpath(os.path.join(path, os.pardir)) != '/':
+        return findConfigFile(name, os.path.join(path, os.pardir))
+
+binary = 'lua-format'
+configFile = findConfigFile(".lua-format", os.path.curdir)
+
+def get_buffer(encoding):
+  if platform.python_version_tuple()[0] == '3':
+    return vim.current.buffer
+  return [ line.decode(encoding) for line in vim.current.buffer ]
+
+def main():
+  # Get the current text.
+  encoding = vim.eval("&encoding")
+  buf = get_buffer(encoding)
+  text = '\n'.join(buf)
+
+  # Determine range to format.
+  if vim.eval('exists("l:lines")') == '1':
+    lines = ['-lines', vim.eval('l:lines')]
+  elif vim.eval('exists("l:formatdiff")') == '1':
+    with open(vim.current.buffer.name, 'r') as f:
+      ondisk = f.read().splitlines();
+    sequence = difflib.SequenceMatcher(None, ondisk, vim.current.buffer)
+    lines = []
+    for op in reversed(sequence.get_opcodes()):
+      if op[0] not in ['equal', 'delete']:
+        lines += ['-lines', '%s:%s' % (op[3] + 1, op[4])]
+    if lines == []:
+      return
+  else:
+    lines = ['-lines', '%s:%s' % (vim.current.range.start + 1,
+                                  vim.current.range.end + 1)]
+
+  # Determine the cursor position.
+  cursor = int(vim.eval('line2byte(line("."))+col(".")')) - 2
+  if cursor < 0:
+    print('Couldn\'t determine cursor position. Is your file empty?')
+    return
+
+  # Avoid flashing an ugly, ugly cmd prompt on Windows when invoking clang-format.
+  startupinfo = None
+  if sys.platform.startswith('win32'):
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    startupinfo.wShowWindow = subprocess.SW_HIDE
+
+  # Call formatter.
+  command = [binary, '-si']
+  if configFile:
+    command.extend(['-c', configFile])
+  p = subprocess.Popen(command,
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                       stdin=subprocess.PIPE, startupinfo=startupinfo)
+  stdout, stderr = p.communicate(input=text.encode(encoding))
+
+  # If successful, replace buffer contents.
+  if stderr:
+    print(stderr)
+
+  if not stdout:
+    print(
+        'No output from clang-format (crashed?).\n'
+        'Please report to bugs.llvm.org.'
+    )
+  else:
+    lines = stdout.decode(encoding).split('\n')
+    sequence = difflib.SequenceMatcher(None, buf, lines)
+    for op in reversed(sequence.get_opcodes()):
+      if op[0] is not 'equal':
+        vim.current.buffer[op[1]:op[2]] = lines[op[3]:op[4]]
+
+main()
