c070f6347016658c93b5a3c857113757c87aa908
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-06-06 23:03:09 +0300

    format code

--- lua-format.py
+++ lua-format.py
@@ -9,26 +9,30 @@ import sys
 import vim
 import os
 
+
 def findConfigFile(name, path):
-    for curFile in os.listdir(path):
-        retval = False
-        if name == curFile:
-            retval = os.path.join(path, name)
-            break
-    if retval:
-        return os.path.abspath(retval)
-    else:
-        parrentPath = os.path.realpath(os.path.join(path, os.pardir))
-        if os.path.split(parrentPath)[1]:
-            return findConfigFile(name, parrentPath)
+  for curFile in os.listdir(path):
+    retval = False
+    if name == curFile:
+      retval = os.path.join(path, name)
+      break
+  if retval:
+    return os.path.abspath(retval)
+  else:
+    parrentPath = os.path.realpath(os.path.join(path, os.pardir))
+    if os.path.split(parrentPath)[1]:
+      return findConfigFile(name, parrentPath)
+
 
 binary = 'lua-format'
 configFile = findConfigFile(".lua-format", os.path.curdir)
 
+
 def get_buffer(encoding):
   if platform.python_version_tuple()[0] == '3':
     return vim.current.buffer
-  return [ line.decode(encoding) for line in vim.current.buffer ]
+  return [line.decode(encoding) for line in vim.current.buffer]
+
 
 def main():
   # Get the current text.
@@ -41,7 +45,7 @@ def main():
     lines = ['-lines', vim.eval('l:lines')]
   elif vim.eval('exists("l:formatdiff")') == '1':
     with open(vim.current.buffer.name, 'r') as f:
-      ondisk = f.read().splitlines();
+      ondisk = f.read().splitlines()
     sequence = difflib.SequenceMatcher(None, ondisk, vim.current.buffer)
     lines = []
     for op in reversed(sequence.get_opcodes()):
@@ -50,8 +54,10 @@ def main():
     if lines == []:
       return
   else:
-    lines = ['-lines', '%s:%s' % (vim.current.range.start + 1,
-                                  vim.current.range.end + 1)]
+    lines = [
+        '-lines',
+        '%s:%s' % (vim.current.range.start + 1, vim.current.range.end + 1)
+    ]
 
   # Determine the cursor position.
   cursor = int(vim.eval('line2byte(line("."))+col(".")')) - 2
@@ -59,6 +65,7 @@ def main():
     print('Couldn\'t determine cursor position. Is your file empty?')
     return
 
+  # Avoid flashing an ugly, ugly cmd prompt on Windows when invoking clang-format.
   startupinfo = None
   if sys.platform.startswith('win32'):
     startupinfo = subprocess.STARTUPINFO()
@@ -69,9 +76,12 @@ def main():
   command = [binary, '-si']
   if configFile:
     command.extend(['-c', configFile])
-  p = subprocess.Popen(command,
-                       stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                       stdin=subprocess.PIPE, startupinfo=startupinfo)
+  p = subprocess.Popen(
+      command,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE,
+      stdin=subprocess.PIPE,
+      startupinfo=startupinfo)
   stdout, stderr = p.communicate(input=text.encode(encoding))
 
   # If successful, replace buffer contents.
@@ -79,10 +89,7 @@ def main():
     print(stderr)
 
   if not stdout:
-    print(
-        'No output from lua-format (crashed?).\n'
-        'Please report to bugs.llvm.org.'
-    )
+    print('No output from lua-format (crashed?).\n')
   else:
     lines = stdout.decode(encoding).split('\n')
     sequence = difflib.SequenceMatcher(None, buf, lines)
@@ -90,4 +97,6 @@ def main():
       if op[0] is not 'equal':
         vim.current.buffer[op[1]:op[2]] = lines[op[3]:op[4]]
 
+
 main()
+
